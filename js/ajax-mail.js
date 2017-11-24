8c14ef408c9a9bd7387a5811b424d793006d6a17
José Quiroz <jo_quiroz@outlook.com>
2017-11-24 10:47:34 -0500

    Js and php update

--- js/ajax-mail.js
+++ js/ajax-mail.js
@@ -40,7 +40,7 @@ $(function() {
 			if (data.responseText !== '') {
 				$(formMessages).text(data.responseText);
 			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+				$(formMessages).text('Oops! Algo salió mal y tu mensaje no pudo ser enviado.');
 			}
 		});
 	});
