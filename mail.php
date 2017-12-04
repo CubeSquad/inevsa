058343aca695027c6fe301c21108c08c257a751c
José Quiroz <jo_quiroz@outlook.com>
2017-12-04 15:33:37 -0500

    404 and mail

--- mail.php
+++ mail.php
@@ -10,44 +10,44 @@
         $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($name) OR empty($subject) OR empty($message) ) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Completa el formulario y vuélvelo a intentar.";
+            echo "Please complete the form and try again.";
             exit;
         }
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "info@inevsa.com";
+        $recipient = "wcastro@inevsa.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
 
         // Build the email content.
-        $email_content = "Nombre: $name\n";
+        $email_content = "Name: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Asunto: $subject\n\n";
-        $email_content .= "Mensaje:\n$message\n";
+        $email_content .= "Subject: $subject\n\n";
+        $email_content .= "Message:\n$message\n";
 
         // Build the email headers.
-        $email_headers = "De: $name <$email>";
+        $email_headers = "From: $name <$email>";
 
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Gracias! Tu mensaje ha sido enviado.";
+            echo "Thank You! Your message has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Algo salió mal y no pudimos enviar tu mensaje.";
+            echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "Hubo un problema con tu solicitud, Inténtalo de nuevo.";
+        echo "There was a problem with your submission, please try again.";
     }
 
 ?>
