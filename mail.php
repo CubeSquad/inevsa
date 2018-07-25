7145efa09c957f316099ccbd978e153a5ceed1a9
José Quiroz <jo_quiroz@outlook.com>
2018-07-25 11:32:27 -0500

    Update

--- mail.php
+++ mail.php
@@ -19,35 +19,35 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "wcastro@inevsa.com";
+        $recipient = "info@inevsa.com";
 
         // Set the email subject.
-        $subject = "New contact from $name";
+        $subject = "Nuevo contacto de $name";
 
         // Build the email content.
-        $email_content = "Name: $name\n";
+        $email_content = "Nombre: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Subject: $subject\n\n";
-        $email_content .= "Message:\n$message\n";
+        $email_content .= "Asunto: $subject\n\n";
+        $email_content .= "Mensaje:\n$message\n";
 
         // Build the email headers.
-        $email_headers = "From: $name <$email>";
+        $email_headers = "De: $name <$email>";
 
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Gracias, tu mensaje ha sido enviado.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Oops! Algo salió mal y no pudimos enviar tu mensaje.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Al parecer hubo un problema con la solicitud, Inténtalo de nuevo por favor .";
     }
 
 ?>
